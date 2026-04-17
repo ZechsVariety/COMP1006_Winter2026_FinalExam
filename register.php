df339b585c6d63ca62611810c09c30fcfc6c2fa1
Zecheriah <98231952+ZechsVariety@users.noreply.github.com>
2026-04-17 12:41:10 -0400

    form field validation
    decided to add the confirm password because it adds to the validation

--- register.php
+++ register.php
@@ -1,5 +1,48 @@
 <?php
     require "includes/connect.php";
+
+    $errors = [];
+    $success = "";
+
+    //form submitted
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        //grab form data
+        $email = trim(filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL));
+        $password = $_POST["password"];
+        $confirmPass = $_POST["confirmPass"];
+    
+        //validate
+
+        //email
+
+        //blank
+        if($email == "") {
+            $errors = "Please enter an email.";
+        }
+        //format
+        elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors = "'" . $email . "' is an invalid email address.";
+        }
+
+        //password
+
+        //blank
+        if($password == "") {
+            $errors = "Please enter a password.";
+        }
+        //minimum length
+        elseif(strlen($password) < 8) {
+            $errors = "Password must be at least 8 characters long.";
+        }
+        //blank confirm password
+        elseif($confirmPass == "") {
+            $errors = "Please confirm your password.";
+        }
+        //passwords don't match
+        elseif($password != $confirmPass) {
+            $errors = "Passwords don't match.";
+        }
+    }
 ?>
 
 <p>Register</p>
@@ -10,6 +53,9 @@
 
     <label for="password">Password</label>
     <input type="password" id="password" name="password" required>
+
+    <label for="confirmPass">Confirm Password</label>
+    <input type="password" id="confirmPass" name="confirmPass" required>
 
     <button type="submit">Register</button>
 </form>
