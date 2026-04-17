79a56f38d9d8cdc2ff46d3759085bae92be7819d
Zecheriah <98231952+ZechsVariety@users.noreply.github.com>
2026-04-17 12:25:05 -0400

    connect.php

---
+++ includes/connect.php
@@ -0,0 +1,26 @@
+<?php
+    //for xampp
+    $host = "localhost";
+    $db = "Final";
+    $user = "root";
+    $password = "";
+
+    //for web server
+    /* $host = "172.31.22.43";
+    $db = "Zech200639774";
+    $user = "Zech200639774";
+    $password = "fMwq4fW4nl"; */
+
+    $dsn = "mysql:host=$host;dbname=$db";
+
+    //try to connect
+    try
+    {
+        $pdo = new PDO($dsn, $user, $password);
+        $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        die("Database connection failed: " . $e->getMessage());
+    }
+?>
